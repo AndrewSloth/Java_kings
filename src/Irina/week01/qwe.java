a9be5a73cd1dd4591e43a839ad781f1a646d2881
Irina <irin.l.naumova@gmail.com>
2023-06-07 00:33:50 -0700

    test

---
+++ src/Irina/week01/qwe.java
@@ -0,0 +1,37 @@
+package Irina.week01;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class qwe {
+
+
+    public static void divider(double num1, double num2) {
+
+        if (num1 < 0 || num2 <= 0) {
+            System.err.println ("Invalid entry");
+            System.exit(1);
+        }
+
+        double num1Entry = num1;
+        int result = 0;
+        NumberFormat formatter = new DecimalFormat("#0.00");
+
+        while (num1 >= num2) {
+            num1 -= num2;
+            result++;
+        }
+
+        System.out.println(num1Entry + " : " + num2 + " = " + result + " (reminder: " + formatter.format(num1) + ")");
+    }
+
+    public static void main(String[] args) {
+        divider(0, 0.25);
+    }
+}
+/*
+ Write a method that can divide two numbers without using division operator
+ */
+
+
+
